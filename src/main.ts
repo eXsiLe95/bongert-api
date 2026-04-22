d40e464331bb5208f17b748e03386786cbf57c78
Lukas Kimpel <mail@lukaskimpel.com>
2026-04-22 19:32:34 +0200

    feat: add database connection to postgres; chore: update postgres to 18.3

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,22 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigType } from '@nestjs/config';
+import { DataSource } from 'typeorm';
 import appConfig from './config/app.config';
 
 async function bootstrap() {
+  const logger = new Logger('Bootstrap');
+
   // Initialize app
   const app = await NestFactory.create(AppModule);
 
   // Instantiate config service
   const config = app.get<ConfigType<typeof appConfig>>(appConfig.KEY);
+  const dataSource = app.get(DataSource);
+
+  await dataSource.query('SELECT 1');
+  logger.log('Database connection verified');
 
   // Start app
   await app.listen(config.port, '0.0.0.0');
